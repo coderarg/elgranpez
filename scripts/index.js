5bacfb25c02b6a0de7cec75c6c32e9fd54c4bf40
Lucas García <lucasgarcia.developer@gmail.com>
2023-09-19 22:11:22 -0300

    Add cargarProductos function

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,9 @@ const categorias = document.querySelectorAll('.selectores__cat');
 const togglemenuFiltros = document.querySelector('#togglemenu--filtros');
 const headerFiltros = document.querySelector('#header--botonFiltros');
 let filtrosOpen = false;
-const productos = [];
+const prodRoot = document.querySelector('#prod-root')
+let productos;
+const numeroResultados = document.querySelector('#numero-resultados');
 
 filtrosBTN.addEventListener('click', (e) => {
   e.preventDefault();
@@ -48,9 +50,59 @@ categorias.forEach((cat) => {
   });
 });
 
-fetch("../recursos/datos/productos.json")
-  .then(res => res.json())
-  .then(json => {
-    productos = [...json]
-    console.log(productos);
+
+fetch("../datos/productos.json")
+    .then(res => res.json())
+    .then(json => {
+        productos = [...json]
+    
+        cargarProductos(productos);
+    })
+
+
+const cargarProductos = (prods) => {
+  prodRoot.innerHTML ='';
+
+  numeroResultados.textContent = activeProds(prods).toString();
+
+
+  
+  prods.forEach( (element) => {
+
+    
+    if(element.activo === 1) {
+
+      const div = document.createElement('div');
+      div.classList.add('producto')
+      
+      div.innerHTML = `
+      <div class="producto__img-container">
+        <img src="./recursos/productos/${element.img1}" alt="${element.titulo}" class="producto__img">
+      </div>
+      <h3 class="producto__titulo">${element.titulo}</h3>
+      <div class="producto__boton">
+        <p class="producto__boton--precio">$ <span id="prod__precio">${element.precio.toString()}</span></p>
+      </div>
+      `;
+      
+      prodRoot.append(div);
+    }
+     
   })
+}
+
+const activeProds = (list) => {
+  const active = list.filter( e => e.activo === 1)
+  return active.length;
+}
+
+const numToPrice = () => {
+  //posibles entradas: 1 / 12 / 123, 1234, 1234.23 / 1234.2 / 123 / 
+  //pasarlo a string
+  //reemplazar el punto . del decimal por una coma ,
+  //si hay 1 número después de la coma, agregar un cero 0
+  //si los enteros son más de mil, agregar un . en los miles
+
+  // Posibles soluciones:
+  //Separar decimales, y miles 
+}
