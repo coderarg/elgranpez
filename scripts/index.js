ac72b87b906f8fff47e37649eb06661cba86ce22
Lucas García <lucasgarcia.developer@gmail.com>
2023-10-07 19:43:26 -0300

    Update javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,26 @@
+/*******************************************
+              DOM y Variables 
+*******************************************/
 import './toggleMenu.js';
-import './filtros.js';
-import './buscar.js';
+const buscarBotton = document.querySelector('#boton__buscar');
+const buscarInput = document.querySelector('#barra__buscar');
+const buscarLupa = document.querySelector('.buscar__logo');
 
-//DOM y Variables
 const numeroResultados = document.querySelector('#numero-resultados');
 const prodRoot = document.querySelector('#prod-root');
-export let prodArray = [];
+let prodArray = [];
+
+const filtrosBTN = document.querySelector('#filtrosBTN')
+const filtros = document.querySelector('#filtros');
+const categorias = document.querySelectorAll('.selectores__cat');
+const togglemenuFiltros = document.querySelector('#togglemenu--filtros');
+const headerFiltros = document.querySelector('#header--botonFiltros');
+let filtrosOpen = false;
 
 
+/*******************************************
+              DOM y Variables 
+*******************************************/
 fetch("../datos/productos.json")
   .then(res => res.json())
   .then(json => {
@@ -15,7 +28,92 @@ fetch("../datos/productos.json")
     cargarProductos(prodArray);
   })
 
-export const cargarProductos = (prods) => {
+
+/*******************************************
+                  Eventos
+*******************************************/
+buscarBotton.addEventListener('click', (e)=> {
+  e.preventDefault();
+  buscarProductos();
+});
+buscarLupa.addEventListener('click', (e)=> {
+  e.preventDefault();
+  buscarProductos();
+});
+
+buscarInput.addEventListener('keyup', (e)=>{
+  if(e.keyCode === 13){
+    buscarProductos();
+  }
+})
+
+filtrosBTN.addEventListener('click', (e) => {
+  e.preventDefault();
+  filtrosOpen ? filtros.style.top = '-1000px' : filtros.style.top = '4px';
+  filtrosOpen = !filtrosOpen;
+});
+
+headerFiltros.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (filtrosOpen) {
+    filtros.style.top = '-1000px';
+    filtrosOpen = false;
+  } else {
+    filtros.style.top = '4px';
+    filtrosOpen = true;
+  };
+});
+
+togglemenuFiltros.addEventListener('click', (e) => {
+  e.preventDefault();
+  toggleMenu.style.left = '100vw';
+  if (filtrosOpen) {
+    filtros.style.top = '-1000px';
+    filtrosOpen = false;
+  } else {
+    filtros.style.top = '4px';
+    filtrosOpen = true;
+  };
+});
+
+categorias.forEach((cat) => {
+  cat.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
+});
+/*******************************************
+              Funciones
+*******************************************/
+const buscarProductos = () => {
+  
+  const palabrasInput = [];
+  const productosDuplicados = [];
+
+  palabrasInput.push(...buscarInput.value.toLowerCase().split(' '));
+
+  palabrasInput.forEach(palabra => {
+    prodArray.forEach(producto => {
+      if(producto.titulo.toLowerCase().includes(palabra)) {
+        productosDuplicados.push(producto);
+      }
+    })
+  })
+  
+  const productosSinDuplicar = productosDuplicados.reduce((sum, prod) => {
+    if(!sum.find(item => item.titulo === prod.titulo)) sum.push(prod);
+    return sum;
+  }, [])
+
+  cargarProductos(productosSinDuplicar)
+  if(prodArray.length == productosDuplicados.length) {
+    document.querySelector('#filtro-resultado').innerText = "todos los productos";
+  }else{
+    document.querySelector('#filtro-resultado').innerText = "resultado de búsqueda";
+  }
+
+}
+
+const cargarProductos = (prods) => {
   prodRoot.innerHTML = '';
 
   numeroResultados.textContent = activeProds(prods).toString();
